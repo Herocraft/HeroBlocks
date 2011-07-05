7f99dfe7acafac7815f99d88d935f93dda0ecc8a
DThielke <dave.thielke@gmail.com>
2011-07-05 15:17:06 -0400

    Useless test commit.

--- src/com/herocraftonline/dthielke/expblocks/HeroBlocks.java
+++ src/com/herocraftonline/dthielke/expblocks/HeroBlocks.java
@@ -6,19 +6,19 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HeroBlocks extends JavaPlugin {
-    
+
     private static final Logger logger = Logger.getLogger("Minecraft");
 
     @Override
     public void onDisable() {
-        
+
     }
 
     @Override
     public void onEnable() {
         log(Level.INFO, "version " + getDescription().getVersion() + " enabled.");
     }
-    
+
     public void log(Level level, String msg) {
         logger.log(level, "[HeroBlocks] " + msg);
     }
